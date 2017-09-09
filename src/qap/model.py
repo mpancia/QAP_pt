95e640494241d2bf9db66059ebef8d02ba1462d7
Matt Pancia <msp@themsp.org>
2017-09-09 14:23:01 -0700

    Fix python problems and imports.

--- src/qap/model.py
+++ src/qap/model.py
@@ -1,21 +1,7 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
-
-import matplotlib
-matplotlib.use('Agg')
-
-# Pytorch requirements
-import unicodedata
-import string
-import re
-import random
-
 import torch
 import torch.nn as nn
-from torch.nn import init
-from torch.autograd import Variable
-from torch import optim
 import torch.nn.functional as F
+from torch.autograd import Variable
 
 if torch.cuda.is_available():
     dtype = torch.cuda.FloatTensor
@@ -24,31 +10,20 @@ else:
     dtype = torch.FloatTensor
     dtype_l = torch.cuda.LongTensor
 
-def sinkhorn_knopp(A, iterations=1):
-    A_size = A.size()
-    for it in range(iterations):
-        A = A.view(A_size[0]*A_size[1], A_size[2])
-        A = F.softmax(A)
-        A = A.view(*A_size).permute(0, 2, 1)
-        A = A.view(A_size[0]*A_size[1], A_size[2])
-        A = F.softmax(A)
-        A = A.view(*A_size).permute(0, 2, 1)
-    return A
 
 def gmul(input):
     W, x = input
     # x is a tensor of size (bs, N, num_features)
     # W is a tensor of size (bs, N, N, J)
-    x_size = x.size()
     W_size = W.size()
     N = W_size[-2]
-    J = W_size[-1]
     W = W.split(1, 3)
-    W = torch.cat(W, 1).squeeze(3) # W is now a tensor of size (bs, J*N, N)
-    output = torch.bmm(W, x) # output has size (bs, J*N, num_features)
+    W = torch.cat(W, 1).squeeze(3)  # W is now a tensor of size (bs, J*N, N)
+    output = torch.bmm(W, x)  # output has size (bs, J*N, num_features)
     output = output.split(N, 1)
-    output = torch.cat(output, 2) # output has size (bs, N, J*num_features)
+    output = torch.cat(output, 2)  # output has size (bs, N, J*num_features)
     return output
+
 
 class Gconv_last(nn.Module):
     def __init__(self, feature_maps, J):
@@ -59,13 +34,14 @@ class Gconv_last(nn.Module):
 
     def forward(self, input):
         W = input[0]
-        x = gmul(input) # out has size (bs, N, num_inputs)
+        x = gmul(input)  # out has size (bs, N, num_inputs)
         x_size = x.size()
         x = x.contiguous()
         x = x.view(x_size[0]*x_size[1], -1)
-        x = self.fc(x) # has size (bs*N, num_outputs)
+        x = self.fc(x)  # has size (bs*N, num_outputs)
         x = x.view(*x_size[:-1], self.num_outputs)
         return W, x
+
 
 class Gconv(nn.Module):
     def __init__(self, feature_maps, J):
@@ -78,16 +54,17 @@ class Gconv(nn.Module):
 
     def forward(self, input):
         W = input[0]
-        x = gmul(input) # out has size (bs, N, num_inputs)
+        x = gmul(input)  # out has size (bs, N, num_inputs)
         x_size = x.size()
         x = x.contiguous()
         x = x.view(-1, self.num_inputs)
-        x1 = F.relu(self.fc1(x)) # has size (bs*N, num_outputs)
+        x1 = F.relu(self.fc1(x))  # has size (bs*N, num_outputs)
         x2 = self.fc2(x)
         x = torch.cat((x1, x2), 1)
         x = self.bn(x)
         x = x.view(*x_size[:-1], self.num_outputs)
         return W, x
+
 
 class GNN(nn.Module):
     def __init__(self, num_features, num_layers, J):
@@ -110,6 +87,7 @@ class GNN(nn.Module):
         out = self.layerlast(cur)
         return out[1]
 
+
 class Siamese_GNN(nn.Module):
     def __init__(self, num_features, num_layers, J):
         super(Siamese_GNN, self).__init__()
@@ -120,11 +98,12 @@ class Siamese_GNN(nn.Module):
         emb2 = self.gnn(g2)
         # embx are tensors of size (bs, N, num_features)
         out = torch.bmm(emb1, emb2.permute(0, 2, 1))
-        return out # out has size (bs, N, N)
+        return out  # out has size (bs, N, N)
+
 
 if __name__ == '__main__':
     # test modules
-    bs =  4
+    bs = 4
     num_features = 10
     num_layers = 5
     N = 8
@@ -156,5 +135,3 @@ if __name__ == '__main__':
     siamese_gnn = Siamese_GNN(num_features, num_layers, J)
     out = siamese_gnn(input1, input2)
     print(out.size())
-
-
